f0c221b18792fc5f87880f5019baf786e91fc934
Matt Gale <matt@littleball.co.uk>
2014-02-10 22:31:21 +0000

    Added gulp

---
+++ gulpfile.js
@@ -0,0 +1,35 @@
+var gulp = require('gulp'),
+    util = require('gulp-util'),
+    browserify = require('gulp-browserify'),
+    concat = require('gulp-concat');
+
+gulp.task('scripts', ['scripts-bundle']);
+
+gulp.task('scripts-bundle', function () {
+  return gulp.src('src/app/game.js')
+    .pipe(browserify({
+      debug: true
+    }))
+    .pipe(concat('bundle.js'))
+    .pipe(gulp.dest('bin/js'));
+});
+
+gulp.task('markup', ['markup-concat']);
+
+gulp.task('markup-concat', function () {
+  return gulp.src('src/**/*.html')
+    .pipe(gulp.dest('bin/'));
+});
+
+gulp.task('data', function () {
+  return gulp.src('src/*.json')
+    .pipe(gulp.dest('bin/'));
+});
+
+gulp.task('compile', ['scripts', 'markup', 'data']);
+
+gulp.task('watch', function () {
+  return gulp.watch('src/**/*', ['compile']);
+});
+
+gulp.task('default', ['compile', 'watch']);
