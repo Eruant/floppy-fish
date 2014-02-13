231d8a7db0c5dabe4cc6982bda599ff053d8f8d4
Matt Gale <matt@littleball.co.uk>
2014-02-13 10:40:05 +0000

    Added fish starting point

--- src/game.js
+++ src/game.js
@@ -7,23 +7,28 @@
 require('SAT/SAT');
 
 var Phaser = require('phaser/phaser'),
-    game = new Phaser.Game(800, 600, Phaser.AUTO, "", {
+    game = new Phaser.Game(480, 320, Phaser.AUTO, "", {
       preload: preload,
       create: create,
       update: update
-    });
+    }),
+    player;
 
 function preload () {
 
-  console.log('pre-load');
   game.load.spritesheet('textures', 'textures.png', 16, 16);
 
 }
 
 function create () {
 
-  console.log('create');
-  game.add.sprite(0, 0, 'textures');
+  // add player and it's settings
+  player = game.add.sprite(32, game.world.height - 150, 'textures');
+
+  // player physics properties
+  player.body.bounce.y = 0.9;
+  player.body.gravity.y = 100;
+  player.body.collideWorldBounds = true;
 
 }
 
